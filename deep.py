e0b6753c23436c654590870cae7693e40f37c33a
Bum81ebe <86529395+Bum81ebe@users.noreply.github.com>
2023-03-09 00:24:26 +0400

    Add files via upload

---
+++ deep.py
@@ -0,0 +1,8 @@
+# task 7 Great Question of Life
+
+question = input(str("What is the Answer to the Great Question of Life, the Universe, and Everything "))
+
+if question == "42" or question == "forty-two" or question == "forty two":
+    print("YES")
+else:
+    print("NO")
